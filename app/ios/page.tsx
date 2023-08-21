35ab8a72c02b26e9dc95da8343457035ea75453d
Vivek Bhupatiraju <vivekab@mit.edu>
2023-08-21 00:24:04 -0400

    cleanup

--- app/ios/page.tsx
+++ app/ios/page.tsx
@@ -7,8 +7,8 @@ import { useState } from "react";
 export default function IosPage() {
   // const generatePass = async () => {
   //   const res = axios.get('/api/generatePass')
-
   // }
+
   const [owner, setOwner] = useState("");
   const [signature, setSignature] = useState(
     "0xa02fe270dd97d121c00373408f4ebc34e1ff45a49eb1068ee4f9e4063da7b3f8"
@@ -21,58 +21,7 @@ export default function IosPage() {
         <h1 className="text-gray-700 text-xl font-bold">
           Create Your Jubmoji 🚀
         </h1>
-        <div className="">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
-          >
-            Username
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            value={owner}
-            onChange={(e) => setOwner(e.target.value)}
-            type="text"
-            placeholder="@username"
-            autoFocus
-          />
-        </div>
-        <div className="mb-2">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="signature"
-          >
-            Signature
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="signature"
-            value={signature}
-            onChange={(e) => setSignature(e.target.value)}
-            type="text"
-            placeholder="Signature"
-          />
-        </div>
-        <div className="mb-2">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="note"
-          >
-            Note
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="note"
-            value={note}
-            onChange={(e) => setNote(e.target.value)}
-            type="text"
-            placeholder="Note (optional)"
-          />
-        </div>
-        <a
-          href={`/api/generatePass?owner=${owner}&signature=${signature}&note=${note}`}
-        >
+        <a href={`/api/generatePass?signature=${signature}`}>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
