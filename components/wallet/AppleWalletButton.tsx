918649a2dbf183ff443b0051cbca222d1dbd4d57
Vivek Bhupatiraju <vivekab@mit.edu>
2023-08-27 20:12:38 -0700

    fixing updating google wallet

--- components/wallet/AppleWalletButton.tsx
+++ components/wallet/AppleWalletButton.tsx
@@ -9,37 +9,36 @@ import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export const AppleWalletButton = () => {
-  const [sigmojis, setSigmojis] = useState<Sigmoji[] | undefined>(undefined);
   const [serial, setSerial] = useState<string | undefined>(undefined);
+  const [number, setNumber] = useState<number | undefined>(undefined);
 
   useEffect(() => {
     loadSigmojis().then((sigmojis) => {
-      setSigmojis(sigmojis);
+      setNumber(sigmojis.length);
     });
     loadBackupState().then((backup) => {
       if (backup !== undefined && backup.type === "apple") {
         setSerial(backup.serialNum);
       } else {
-        const backup: BackupState = {
-          type: "apple",
-          serialNum: uuidv4(),
-        };
-        saveBackupState(backup);
-        setSerial(backup.serialNum);
+        setSerial(uuidv4());
       }
     });
   });
 
   return (
     <div className="flex justify-center">
-      <a
+      <button
         style={{ margin: 0 }}
-        href={
-          sigmojis && serial
-            ? `api/generateApplePass?number=${sigmojis.length.toString()}` +
-              `&serial=${serial}`
-            : "#"
-        }
+        onClick={() => {
+          if (number && serial) {
+            saveBackupState({
+              type: "apple",
+              serialNum: serial,
+            });
+            window.location.href = `/api/generateApplePass?number=${number}&serial=${serial}`;
+          }
+        }}
+        disabled={!(number && serial)}
       >
         <Image
           src="/buttons/AddToAppleWallet.svg"
@@ -47,9 +46,9 @@ export const AppleWalletButton = () => {
           width={201}
           height={42}
           sizes="100vw"
-          style={sigmojis ? {} : { filter: "grayscale(100%)" }}
+          style={number && serial ? {} : { filter: "grayscale(100%)" }}
         />
-      </a>
+      </button>
     </div>
   );
 };
