310a5bc6af2d24e8e2716253507a1f6f605386a3
Vivek Bhupatiraju <vivekab@mit.edu>
2023-09-02 17:26:32 -0700

    added in second set of card pub keys

---
+++ lib/cardPubKeys2.ts
@@ -0,0 +1,282 @@
+export const cardPubKeys2 = {
+  "0x4bd4cf685e0cc3561a45a5f0af70241ec836a33255b73acef28ec7a6ac8d01da": {
+    primaryPublicKeyRaw:
+      "04fc5d86b9d83c901e96551b612701f65138823f666fd28e7b2547f8f0b146076ca9113702f646b8fdef91610059cf561973fa98e10070ba8ba548503bc7902e10",
+    primaryPublicKeyHash:
+      "0x4bd4cf685e0cc3561a45a5f0af70241ec836a33255b73acef28ec7a6ac8d01da",
+    secondaryPublicKeyRaw:
+      "04814583328d53c99394b741354a7aa1f7da7eb9ad050e453540c5aef0d5316b451bc884559650a88a743f35677f0f5c4e67180553a651ef53e252c68c73a79ce3",
+    secondaryPublicKeyHash:
+      "0x7955ab214a7a2d59244c5aeb76fed789dfa478cfc77615584cc525dd2189188d",
+    tertiaryPublicKeyHash: null,
+    address: "0x5B7961D5a28D3aaf6339Bc438C89d5824b668Cf5",
+    edition_number: 1,
+    emoji: "🤯",
+  },
+  "0x74820eb5539f0f0cea20a4e3a6f42af0c2a864d09844fa9a87cf0c407bc9dcfa": {
+    primaryPublicKeyRaw:
+      "0416bc6092bacf26781c7536ab7c70d910dc1a7823f5cdb46a9a081843bbfef90390748b66e97f2cdce309fe497986b3ade17f30a506b74b2e33714989bad81796",
+    primaryPublicKeyHash:
+      "0x74820eb5539f0f0cea20a4e3a6f42af0c2a864d09844fa9a87cf0c407bc9dcfa",
+    secondaryPublicKeyRaw:
+      "04ae5b0ebf63cecd39cee620712c8e490918eb269aecd173a5ec04d381722df569b472653e98972e15aad4fa6f58287c5e576220d2aeb43ed0294cadc9df08abac",
+    secondaryPublicKeyHash:
+      "0x95420d5b2b40066aab9552840b8ea85f9793959517cbe3b23599cb804d135c86",
+    tertiaryPublicKeyHash: null,
+    address: "0xc026D86156776919E701CA2B8B3a0B8Fbe75721B",
+    edition_number: 2,
+    emoji: "🐿",
+  },
+  "0xd356c4371f6a10e6ff0d73dbfb811057990d7e7cdb807119cf963bd22fe056e5": {
+    primaryPublicKeyRaw:
+      "0492822f375cffb3af4aebe6b263f96da645ac871f78df88744264dc5f6ee5379772977e13ecd5d155aef56adfeda1e88b9b7094765dcefb2dd01e23c9a6899665",
+    primaryPublicKeyHash:
+      "0xd356c4371f6a10e6ff0d73dbfb811057990d7e7cdb807119cf963bd22fe056e5",
+    secondaryPublicKeyRaw:
+      "040b5950039a5b7f5d60b1b17e4e688ab263225a0d0fc164865cf947cec1366acb9d46eef0dad397c29f4e7f62076c7d1dd04c1b6612dd6e4b3d5fa9f0db117c76",
+    secondaryPublicKeyHash:
+      "0x6da324fa6a8d74beb9576880793c91a22028d6809ca8517f222edb3f3607fd9c",
+    tertiaryPublicKeyHash: null,
+    address: "0x6476770355E549bc308E7715e3Fb99a503996180",
+    edition_number: 3,
+    emoji: "🍪",
+  },
+  "0x26fd28ade9ee435cb49adc8167a13d5e6cae50e0ebf12515a51311b482d47ff7": {
+    primaryPublicKeyRaw:
+      "04af6d0eae5fd2b561fb5a86382808ea137d5b2e5df92184a6990e5d5c672db940e2282c70359354987437e9fbb1007e395d05629f8f9888492107f5299a11321d",
+    primaryPublicKeyHash:
+      "0x26fd28ade9ee435cb49adc8167a13d5e6cae50e0ebf12515a51311b482d47ff7",
+    secondaryPublicKeyRaw:
+      "042b9603d4195eecf79519222d299c55c170a097892fa401ad894174fd43144a87b5484c6a4d200517248ea6907100baac0c3e13a495aedf0015b4e73d7a91c800",
+    secondaryPublicKeyHash:
+      "0x8d06735d24c4b0a676b508ddac797ea844b1c529a23d5988f00f1cdb8af19e36",
+    tertiaryPublicKeyHash: null,
+    address: "0xc9d8F0A18D8056E7B3F3729cb5C939DeF8E05b45",
+    edition_number: 4,
+    emoji: "🔮",
+  },
+  "0xf4aaf38cbcc745e673e27d7abe9b91c47035e27b46adc849ed4d37d831161f13": {
+    primaryPublicKeyRaw:
+      "04399d894a766c3f66122234c608a5ebd508b16dcc3b092d6b8ac18e3fde4061b31799d26419cf476fe334831296429bdf807b2f02661cf8f4eb2938c062ec4958",
+    primaryPublicKeyHash:
+      "0xf4aaf38cbcc745e673e27d7abe9b91c47035e27b46adc849ed4d37d831161f13",
+    secondaryPublicKeyRaw:
+      "040306bbf644e9df5e9c624d7d7b35d28dfbc489d4cdc158cf2c9d8ba9cb8771bc69e8c8c6d8b9a53d6ae1066d519fb0d7c66b72776bcfbde1034efebb9e923544",
+    secondaryPublicKeyHash:
+      "0x5a4b1bbb413a9ec60cf38e71f062740ed65cde58027b99d18baa94c95fefa735",
+    tertiaryPublicKeyHash: null,
+    address: "0x1db42299d25950Ae2f0CCbA2e578a2932dFEAB4D",
+    edition_number: 5,
+    emoji: "🪄",
+  },
+  "0x2b8ec41fb0d140fe82558aff77ec1af11b245d7b7ef40030cdc6d9403a879c90": {
+    primaryPublicKeyRaw:
+      "04400c35ec256f8500bfb3f2dc4d74c2613747290d9f890a3f1ed35bf2b1059a3c4604195ba2498370535531dce2086d9d792feebe24233afae37ea79ffbfbbb2a",
+    primaryPublicKeyHash:
+      "0x2b8ec41fb0d140fe82558aff77ec1af11b245d7b7ef40030cdc6d9403a879c90",
+    secondaryPublicKeyRaw:
+      "04253eb83fdd65e35b32dd8310e5938852a537e25e5ac56075a1c0138d3d47f77af19439f4789fdd6a94a4c18c5ea968d9af81a4db65e61b9281d512f132d6d903",
+    secondaryPublicKeyHash:
+      "0x52b7abc1580400ce7d9260f8b5ef1c7192124c864c59f92323b31f64a15f7fe9",
+    tertiaryPublicKeyHash: null,
+    address: "0xD976649C0d4545D52AfB2C05A2eab5f06c400d40",
+    edition_number: 6,
+    emoji: "🧋",
+  },
+  "0x022196bb3695077556dcd193e6b5b8320f18c6f7d8ac5ad2b4574c7aca1b458e": {
+    primaryPublicKeyRaw:
+      "041fdb4c1fc722b6c9d9aa34f678b7f6c7a31a626b32967e42e4177f579d6eea0ca4d8f926a3851647d58c679aa2d33d92aebe267c4b8a8f717482e180f1482922",
+    primaryPublicKeyHash:
+      "0x022196bb3695077556dcd193e6b5b8320f18c6f7d8ac5ad2b4574c7aca1b458e",
+    secondaryPublicKeyRaw:
+      "040e1edaf138c82fdee96a2e8f0584a6c44c8d083e34bbe4c7302423679e3d236113077233def82994f1c6e19467427080b2ee8623c7e9e8f7ba26df7c9ca1d6da",
+    secondaryPublicKeyHash:
+      "0x44e8da5faa55865898660216543de1e866a361526733b2de69ad7ae93a9b0dbf",
+    tertiaryPublicKeyHash: null,
+    address: "0x759Db844706f0924De09450DDF5e52F1C9a17E45",
+    edition_number: 7,
+    emoji: "💀",
+  },
+  "0x05560c2209790f5e2283f91bba7572c9d28632a3ca504f55a3b315f066c5c95e": {
+    primaryPublicKeyRaw:
+      "0474ee192b8306b643b69940384b5978edc7b66ebdfaea7e73b9031f2f8da51831ef6b130e0dc7b09840edcff052eeea3ccfe202e24c8c08a7d759e514aac47af2",
+    primaryPublicKeyHash:
+      "0x05560c2209790f5e2283f91bba7572c9d28632a3ca504f55a3b315f066c5c95e",
+    secondaryPublicKeyRaw:
+      "04f5671f45076b3cffb5cc39b3e2467015dd3fcec395d07ce630626e1388f43431b992b0f8521c12ee0981c8dc17c6e899bb4906d9757569cff950937b08abf015",
+    secondaryPublicKeyHash:
+      "0xc3ab1346bd5236f05f8f26a7b35afb3be1938b65192a77a1a2052c3157cdd0d4",
+    tertiaryPublicKeyHash: null,
+    address: "0x27338ba55F40B568E62E73EEd2CF02025d57088C",
+    edition_number: 8,
+    emoji: "🦥",
+  },
+  "0x9128b96b008cb3ae92bef20b3a39ba8bbb35d729a66d51cf99f657124de3174a": {
+    primaryPublicKeyRaw:
+      "04e08e95e56ebd78b314a85472f33e5477a4ece82638b9ebaf512a2e84af0d86251e47eeef25d47d60766b3bb58e4d83f3d2ab2e6eaf7fa27e2df8bfac03d7e9ac",
+    primaryPublicKeyHash:
+      "0x9128b96b008cb3ae92bef20b3a39ba8bbb35d729a66d51cf99f657124de3174a",
+    secondaryPublicKeyRaw:
+      "04fb555ebebc95a418838616f3e551eab14feb0464906c611aa8144b0b11259b8e5f81d2ef92cdc022ce017c9e3d3952fcc05ccedb9b83ea4da10552fd3bf44f1e",
+    secondaryPublicKeyHash:
+      "0x620e18a0ea1f44017a431413f783257ec4ec48a2758ac87abafa256740b34dbf",
+    tertiaryPublicKeyHash: null,
+    address: "0xA7089Ea555a5342DdeCf659B7179849a5206399E",
+    edition_number: 9,
+    emoji: "👀",
+  },
+  "0x97f86cd238c3d811e6bb9d55419c9bd3c3a131b925ddbdee4694efc5ac94de31": {
+    primaryPublicKeyRaw:
+      "04dcc931fc832975a9d7a0b9c7254f934a323a79d4a18bfc1a8bcfce109f02171532e573ba02a86cda2c0193e4843ecf56ccb8a87f37a45395d81e78f34ab68294",
+    primaryPublicKeyHash:
+      "0x97f86cd238c3d811e6bb9d55419c9bd3c3a131b925ddbdee4694efc5ac94de31",
+    secondaryPublicKeyRaw:
+      "04c20cc07e19fe0412c232ae69589f8c4f7e0dd9f5443df9ee8fd0ef7d8cfc9092fdfb8ceae259ec5a3fd1c8143c8377b215845b21ca89d5b2a2324b9231623b62",
+    secondaryPublicKeyHash:
+      "0x56c91c2d14db4c0de55d070e3064056f9d6b370ad0d09b404854647ee522404e",
+    tertiaryPublicKeyHash: null,
+    address: "0x16cDC497fEadcA0F8EFd4524ea75F711dDdcfC33",
+    edition_number: 10,
+    emoji: "🤝",
+  },
+  "0x4723c0a33b3d0cce47c49dc57a4ec79359a5a05abae53a61864f58a6b51c5ed0": {
+    primaryPublicKeyRaw:
+      "043896ab6920d04a4e43e7f810f3dbf0732b5d872718abe0d94e1dc25e7a1a01f688e8363489c1acfdf6a8a6ad9f0d929eb5c746494de8389bf9e674ad7b6dfc92",
+    primaryPublicKeyHash:
+      "0x4723c0a33b3d0cce47c49dc57a4ec79359a5a05abae53a61864f58a6b51c5ed0",
+    secondaryPublicKeyRaw:
+      "04b679ebcd6b328b0654fd040b7f327efae2f14a6dd20e103013c90f527212900e640a8fc1e9b595feb3db2d69ba477dd00638b078b76501d0183187639a6fd376",
+    secondaryPublicKeyHash:
+      "0x4535b2a4a0bf513a690b87a6dc4f1550cad32e2246b546277040366ee120e5b3",
+    tertiaryPublicKeyHash: null,
+    address: "0x5A65AFe11008bfb2860551cc9aee290E90146561",
+    edition_number: 11,
+    emoji: "blue_swirl",
+  },
+  "0xba6aa9695426ce050957d2bc85d3e0665e224d48dae4f31a9d93d61b81542987": {
+    primaryPublicKeyRaw:
+      "049ee6e7e06dad62ab2c74017f3044827c8c9913664db0767e10c6e875606c98e42ac5314bb319d38abf96a0fc6079f23e89b40db89a08e988b4316716fa5e4ab4",
+    primaryPublicKeyHash:
+      "0xba6aa9695426ce050957d2bc85d3e0665e224d48dae4f31a9d93d61b81542987",
+    secondaryPublicKeyRaw:
+      "04ddaf8c2c9c6f12db6455d5cdcae27cf92551f574d1fdeca48aa7ba19c54e9b5d6f7089d825fff4d9918509c026b2fa19ecb05286f8c268e84ae0cc926cceac6d",
+    secondaryPublicKeyHash:
+      "0x52963b71317db073a369f8d5ecc629761378c386dfef6a9224becfc701cd0729",
+    tertiaryPublicKeyHash: null,
+    address: "0xc361dE30041e7190f201705dC475D9184Def555e",
+    edition_number: 12,
+    emoji: "🍭",
+  },
+  "0x278ea05bd99ac4816163c11bccefd7adabed0bb93362b8efc5c434c62fcfbe5b": {
+    primaryPublicKeyRaw:
+      "04939aa8768998da82bad7f1d36de73864dade1519d5f8451ac7f66f591d4f3e8459f09050cbb0deb889ca733dfba4ce0e434f7ac31649e49641b32583b26d983f",
+    primaryPublicKeyHash:
+      "0x278ea05bd99ac4816163c11bccefd7adabed0bb93362b8efc5c434c62fcfbe5b",
+    secondaryPublicKeyRaw:
+      "04aacecf1aac4584a56601a4864588549804d4b17b421296b30305b2bfececbd5975620f5d4f3902ce4c5cccbd3561910a7afe7e7c190091e8ec8af307e5e36d9b",
+    secondaryPublicKeyHash:
+      "0x2e9cafee1674327247c12ecbc87f31984471b972a9b799707cef7f3220065f28",
+    tertiaryPublicKeyHash: null,
+    address: "0xC0754c73e63F5D518C9e9eF4024E8e2C3EDe1A38",
+    edition_number: 13,
+    emoji: "😾",
+  },
+  "0xc71549c89253932683a9b590870266bd3211a783dc87a0f5297b148b3d028b55": {
+    primaryPublicKeyRaw:
+      "0483bdf60adc283fb868bfe4647d2f22b23bc4d4b1d291a0588217b082bff271e4462b06248a011d3a36a2b6709bb31b9fb28560bb3fd9d86eff1d4f10762fd2e3",
+    primaryPublicKeyHash:
+      "0xc71549c89253932683a9b590870266bd3211a783dc87a0f5297b148b3d028b55",
+    secondaryPublicKeyRaw:
+      "0439f5dde559078ec8355c360f0dba6ef95b7ab2528810bac2cfd491ca3a950e9a3754195712ad158e5e3f9551d8e0b51cd9017438931d1a4d5db8336a946b2cc5",
+    secondaryPublicKeyHash:
+      "0x2f8fc4fff027c37409ad5ec4dc8428db4027e71a084be0c02ed946a134fb50fb",
+    tertiaryPublicKeyHash: null,
+    address: "0xC0741485866382D61A99F077Ca5eB323fFE8Fc58",
+    edition_number: 14,
+    emoji: "💩",
+  },
+  "0x8f64b5c51bd725359354085f4c77819bad49c8bba6f07b7f784d0fa30c860fb6": {
+    primaryPublicKeyRaw:
+      "04f78e88165c5c22066110acf7e8a999953ec0be09e04d9b763f564ce46e4d38760010f5d6f43734e6d2e9b02d66bc3e2425c955552069050a6cc844cdaa1c046f",
+    primaryPublicKeyHash:
+      "0x8f64b5c51bd725359354085f4c77819bad49c8bba6f07b7f784d0fa30c860fb6",
+    secondaryPublicKeyRaw:
+      "04c5d692686843af56b8381375238dba00782b449be014d17e4b33f731ad46ead39b783f6d53c42ad5448732ab28b2b98f63b474a3378b9fb191595e2249b6a30e",
+    secondaryPublicKeyHash:
+      "0xa606240f1d0c49ebad5be3808234f19ae02a1a5cd0e3da97372778cb806051fe",
+    tertiaryPublicKeyHash: null,
+    address: "0x4902164d7D2459f18C801dD91Ee7703c878b51AA",
+    edition_number: 15,
+    emoji: "🤞",
+  },
+  "0x92705b9508c360cfd91cccf9906681ea1ba9a90bd3b59ac45f2ffd103426f9dd": {
+    primaryPublicKeyRaw:
+      "04f832d3f1820e871ab1ee4230cd9e7e64ca2c36a4bd1ea14b573a86c2293b8ca1dc12796c138ebb5c2555f803b29fe8b706dfbfd943ba6ffb9f5861c41676c4e2",
+    primaryPublicKeyHash:
+      "0x92705b9508c360cfd91cccf9906681ea1ba9a90bd3b59ac45f2ffd103426f9dd",
+    secondaryPublicKeyRaw:
+      "041953b2ad1c29d883284d7b0af20467d79b322f4d0091b2c231c361ad16af9ecc2801c117b3a0ca1bf2db045a378687e83ae88278069fe4850f14d13c41c74b02",
+    secondaryPublicKeyHash:
+      "0x198e6065be9f14fe0571dd70ce87a7dc89a89f10b2f562c3d8094ed1cde95331",
+    tertiaryPublicKeyHash: null,
+    address: "0x46C79830a421038E75853eD0b476Ae17bFeC289A",
+    edition_number: 16,
+    emoji: "👼",
+  },
+  "0x72a5c4abdabb0548817ac3532ac01b48320b798568eae78c086511da3375ff28": {
+    primaryPublicKeyRaw:
+      "04c695bc8db617a6f3b51f4acd1966a9b735c08217f303893190a9c2fcb5414595eb5991797e384b924f328ce419f7abeabc4eedd1f43f2efb2a5d4ef2a3967012",
+    primaryPublicKeyHash:
+      "0x72a5c4abdabb0548817ac3532ac01b48320b798568eae78c086511da3375ff28",
+    secondaryPublicKeyRaw:
+      "0496de2db759c0a6bd0b7ac3dc256cfa95ab4a1b4e4554e1be687a6af2c0864afa82bffd0989cc7eeeec9e404267c956ac9e46bd696661b6c28536a2d45c0ec876",
+    secondaryPublicKeyHash:
+      "0x2bcd55b89ef363281504594025f5a96c8c515c07d152440509081553b974d723",
+    tertiaryPublicKeyHash: null,
+    address: "0x0F623230f8ec5c01Ac75a3DD3545F62887F8F9f3",
+    edition_number: 17,
+    emoji: "🤡",
+  },
+  "0x3c7b9c4646f439ede21f7f7baeca346d44823cbb0e24e04a17abe86232360691": {
+    primaryPublicKeyRaw:
+      "041b7d3061a367103a5df8bc245d82be04bfbce82ace8b7d3c83db7691f4cb645057d251d88565fff9dacdb320ff973f919ea878e07fa95ba725dfed7ac4b0e2f7",
+    primaryPublicKeyHash:
+      "0x3c7b9c4646f439ede21f7f7baeca346d44823cbb0e24e04a17abe86232360691",
+    secondaryPublicKeyRaw:
+      "04d200e99fec98599473167224c29116003828448e129bf7721d755e6ef98188d16f6f786d1b79ae5e2336007cd4dd298689d4619550d38f2fb8d07526feecee9f",
+    secondaryPublicKeyHash:
+      "0x683e3c6bfe4ea466b7370f9d0fb6b374b138033bb55e9dcce0ce21953f94dcc1",
+    tertiaryPublicKeyHash: null,
+    address: "0x265C8a8ba20048f841fAa6571663c62a6918f920",
+    edition_number: 18,
+    emoji: "🐛",
+  },
+  "0x925481c7a3c1cf0044613fe16e3bbed965a4ac0900a75358dbae01e032366398": {
+    primaryPublicKeyRaw:
+      "04953a5211ddd2924293c6d256412e74724df71450239cd24757645e7a680e286f7ccc1ddc8e2d569d4db54e2c4b461152af9127a311ba9b59617cf444a53dbe46",
+    primaryPublicKeyHash:
+      "0x925481c7a3c1cf0044613fe16e3bbed965a4ac0900a75358dbae01e032366398",
+    secondaryPublicKeyRaw:
+      "04060174ff00c6b93b0add32243573b90fad96fe7ce7ed396cef49f77616b3a682c1a6e7aa28d02cce4449102c742dfcfd673d846d054e82f05871c6bd4853aa36",
+    secondaryPublicKeyHash:
+      "0xaa788300a57a2819a985ed5941c846845ece957b70a0359d8209b696873f7e2b",
+    tertiaryPublicKeyHash: null,
+    address: "0x6188bB7d7FaC0FB97a6AdE6f739815757A1F09c6",
+    edition_number: 19,
+    emoji: "🎨",
+  },
+  "0x828b975546750653c0d0e058a412c1e5972677541aef84242380a2fa4fb798ea": {
+    primaryPublicKeyRaw:
+      "0480b5389295899076d8eebc749d84e2bedf1d7ecda6180e9e4ce59b736861711df0dcffd102f3d039e9ba7337cc7b20f97c1d4f867cc1fe6ec6c105c6cba8f27e",
+    primaryPublicKeyHash:
+      "0x828b975546750653c0d0e058a412c1e5972677541aef84242380a2fa4fb798ea",
+    secondaryPublicKeyRaw:
+      "04a3ae5686a386576dc4c0ad7a670c96582ecef62435e36ee8267d23ed1af6f286b84005a67d46c23ff45ed1f1211622da8e3b6d2ebeb14acba8b65a2073c68402",
+    secondaryPublicKeyHash:
+      "0xe6a20adbcbfbc2682eaae8ff92f8f163fdd500c290f9996520f4fef1a4c3fafa",
+    tertiaryPublicKeyHash: null,
+    address: "0x87FF0334D89C6EC537e7c4e77DceeA3b05D63B9C",
+    edition_number: 20,
+    emoji: "🥟",
+  },
+};
