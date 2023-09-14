afb72b73d5ac9c2c0305646f55ebc0f9bfd9436e
Vivek Bhupatiraju <vivekab@mit.edu>
2023-09-14 22:38:09 +0200

    add to apple wallet

--- components/wallet/AppleWalletButton.tsx
+++ components/wallet/AppleWalletButton.tsx
@@ -2,8 +2,8 @@ import {
   loadBackupState,
   loadSigmojis,
   saveBackupState,
+  loadSigmojiWalletBackup,
 } from "@/lib/localStorage";
-import { BackupState, Sigmoji } from "@/lib/types";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
@@ -11,10 +11,16 @@ import { v4 as uuidv4 } from "uuid";
 export const AppleWalletButton = () => {
   const [serial, setSerial] = useState<string | undefined>(undefined);
   const [number, setNumber] = useState<number | undefined>(undefined);
+  const [sigmojiWalletBackup, setSigmojiWalletBackup] = useState<
+    string | undefined
+  >(undefined);
 
   useEffect(() => {
     loadSigmojis().then((sigmojis) => {
       setNumber(sigmojis.length);
+    });
+    loadSigmojiWalletBackup().then((serializedSigmojis) => {
+      setSigmojiWalletBackup(serializedSigmojis);
     });
     loadBackupState().then((backup) => {
       if (backup !== undefined && backup.type === "apple") {
@@ -30,12 +36,12 @@ export const AppleWalletButton = () => {
       <button
         style={{ margin: 0 }}
         onClick={() => {
-          if (number && serial) {
+          if (number && serial && sigmojiWalletBackup) {
             saveBackupState({
               type: "apple",
               serialNum: serial,
             });
-            window.location.href = `/api/generateApplePass?number=${number}&serial=${serial}&collection=${window.localStorage["sigmojis"]}}`;
+            window.location.href = `/api/generateApplePass?number=${number}&serial=${serial}&collection=${sigmojiWalletBackup}`;
           }
         }}
         disabled={!(number && serial)}
