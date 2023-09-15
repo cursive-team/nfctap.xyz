da4e073cf384a2de025c62d9d2805bcfaf796c9e
Vivek Bhupatiraju <vivekab@mit.edu>
2023-09-16 00:26:40 +0200

    added tracking the # of sigmojis that have been proven already

--- components/modals/ProvingModal.tsx
+++ components/modals/ProvingModal.tsx
@@ -11,11 +11,21 @@ import { Input } from "../shared/Input";
 import { PrimaryLargeButton } from "../shared/Buttons";
 import { OuterContainer, InnerContainer } from "../shared/Modal";
 import Modal from "./Modal";
-import { ProverWasm, initWasm, makeProofs } from "@/lib/zkProving";
+import {
+  ProverWasm,
+  initWasm,
+  setupTree,
+  addZKPToSigmoji,
+} from "@/lib/zkProving";
+import { loadSigmojis } from "@/lib/localStorage";
 
 export default function ProvingModal() {
   const [wasm, setWasm] = useState<ProverWasm>();
   const [pseudonym, setPseudonym] = useState<string>("");
+
+  const [startedProving, setStartedProving] = useState<boolean>(true); // TODO: [startedProving, setStartedProving
+  const [score, setScore] = useState<number>(0);
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     async function setup() {
@@ -26,6 +36,27 @@ export default function ProvingModal() {
 
     setup();
   });
+
+  const makeProofsWithCounter = async () => {
+    if (!wasm) return;
+
+    setStartedProving(true);
+
+    const pubKeyTree = setupTree(wasm);
+    const sigmojis = await loadSigmojis();
+    setScore(sigmojis.length);
+    const sigmojiPromises = sigmojis.map((sigmoji) =>
+      addZKPToSigmoji(sigmoji, wasm, pubKeyTree)
+    );
+    const wrappedSigmojiPromises = sigmojiPromises.map((p) =>
+      p.then((result) => {
+        setCounter((prevCounter) => prevCounter + 1);
+        return result;
+      })
+    );
+
+    await Promise.all(wrappedSigmojiPromises);
+  };
 
   return (
     <Modal>
@@ -47,9 +78,11 @@ export default function ProvingModal() {
 
           <Input header="Pseudonym" value={pseudonym} setValue={setPseudonym} />
 
-          <PrimaryLargeButton onClick={() => (wasm ? makeProofs(wasm) : {})}>
+          <PrimaryLargeButton
+            onClick={() => (wasm ? makeProofsWithCounter() : {})}
+          >
             <PrimaryFontBase1>
-              {wasm ? "PROVE IT!" : "Loading..."}
+              {wasm && !startedProving ? "PROVE IT!" : "Loading..."}
             </PrimaryFontBase1>
           </PrimaryLargeButton>
         </InnerContainer>
