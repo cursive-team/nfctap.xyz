e842cdc7e057b85099cfadad9e789b08effe6fd4
Vivek Bhupatiraju <vivekab@mit.edu>
2023-09-15 11:59:58 +0200

    add input field + small leaderboard API edit

--- components/modals/ProvingModal.tsx
+++ components/modals/ProvingModal.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { PrimaryFontH3, PrimaryFontBase1 } from "../core";
+import {
+  PrimaryFontH1,
+  PrimaryFontH3,
+  PrimaryFontBase,
+  PrimaryFontBase1,
+} from "../core";
+import { Input } from "../shared/Input";
 import { PrimaryLargeButton } from "../shared/Buttons";
 import { OuterContainer, InnerContainer } from "../shared/Modal";
 import Modal from "./Modal";
@@ -9,6 +15,7 @@ import { ProverWasm, initWasm, makeProofs } from "@/lib/zkProving";
 
 export default function ProvingModal() {
   const [wasm, setWasm] = useState<ProverWasm>();
+  const [pseudonym, setPseudonym] = useState<string>("");
 
   useEffect(() => {
     async function setup() {
@@ -24,9 +31,22 @@ export default function ProvingModal() {
     <Modal>
       <OuterContainer>
         <InnerContainer>
-          <PrimaryFontH3 style={{ color: "var(--woodsmoke-100)" }}>
-            Your score
-          </PrimaryFontH3>
+          <div className="flex-col justify-center items-center gap-2 inline-flex">
+            <PrimaryFontH3 style={{ color: "var(--woodsmoke-100)" }}>
+              Your score
+            </PrimaryFontH3>
+            <PrimaryFontH1 style={{ color: "var(--snow-flurry-200)" }}>
+              832
+            </PrimaryFontH1>
+          </div>
+
+          <PrimaryFontBase style={{ color: "var(--woodsmoke-100)" }}>
+            Create a pseudonym and make a zk proof to share it on the
+            leaderboard.
+          </PrimaryFontBase>
+
+          <Input header="Pseudonym" value={pseudonym} setValue={setPseudonym} />
+
           <PrimaryLargeButton onClick={() => (wasm ? makeProofs(wasm) : {})}>
             <PrimaryFontBase1>
               {wasm ? "PROVE IT!" : "Loading.."}
