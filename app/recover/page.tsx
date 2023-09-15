1be957e17a73b3111340d3daca0f8ecf54144f05
AndrewCLu <andrewclu98@gmail.com>
2023-09-15 14:46:31 -0400

    add recovery page

--- app/recover/page.tsx
+++ app/recover/page.tsx
@@ -1,7 +1,39 @@
 "use client";
 
-import HomeScreen from "@/components/screens/HomeScreen";
+import { useEffect } from "react";
+import { useRouter, useParams } from "next/navigation";
+import { Sigmoji, deserializeSigmoji } from "@/lib/types";
+import { updateSigmojiList } from "@/lib/localStorage";
 
-export default function Home() {
-  return <HomeScreen />;
+export default function RecoverPage() {
+  const router = useRouter();
+  const params = useParams();
+
+  useEffect(() => {
+    const recoverBackup = async () => {
+      if (typeof window !== "undefined") {
+        const urlParams = new URLSearchParams(location.search);
+        const sigmojis = await getCollectionFromParams(urlParams);
+        await updateSigmojiList(sigmojis);
+        router.push("/home");
+      }
+    };
+    recoverBackup();
+  }, [params, router]);
+
+  return <></>;
+}
+
+function getCollectionFromParams(params: URLSearchParams): Promise<Sigmoji[]> {
+  const collection = params.get("collection");
+  if (!collection) {
+    return Promise.resolve([]);
+  }
+
+  const serializedSigmojis = JSON.parse(collection);
+  if (!Array.isArray(serializedSigmojis)) {
+    return Promise.resolve([]);
+  }
+
+  return Promise.all(serializedSigmojis.map(deserializeSigmoji));
 }
