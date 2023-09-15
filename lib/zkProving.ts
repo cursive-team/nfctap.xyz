728eeb9736fec942952a50a582b334d89fc31f2b
Vivek Bhupatiraju <vivekab@mit.edu>
2023-09-15 18:05:49 +0200

    refactor out making zkps

--- lib/zkProving.ts
+++ lib/zkProving.ts
@@ -8,7 +8,7 @@ import { loadSigmojis, updateSigmoji } from "@/lib/localStorage";
 import { cardPubKeys } from "./cardPubKeys";
 import { importPublic } from "@ethereumjs/util";
 import { sha256 } from "js-sha256";
-import { serializeSigmojiZKP } from "@/lib/types";
+import { Sigmoji, serializeSigmoji, serializeSigmojiZKP } from "@/lib/types";
 import {
   parseDERSignature,
   findV,
@@ -34,6 +34,71 @@ export async function initWasm(): Promise<ProverWasm> {
   return { poseidon, prover };
 }
 
+export const addZKPToSigmoji = async (
+  sigmoji: Sigmoji,
+  proverWasm: ProverWasm,
+  pubKeyTree: Tree
+): Promise<Sigmoji> => {
+  if (sigmoji.ZKP !== "") {
+    // set up signature
+    let { r, s } = parseDERSignature(sigmoji.PCD.proof.cleanedSignature);
+
+    // remove 00 at start of r, s if present
+    if (r.length == 66) {
+      r = r.slice(2);
+    }
+    if (s.length == 66) {
+      s = s.slice(2);
+    }
+
+    // setup pubkey + merkle proof
+    const secondaryPublicKeyRawUint8Array = hexToUint8Array(
+      sigmoji.PCD.claim.pubkeyHex
+    );
+    const pubKey = importPublic(secondaryPublicKeyRawUint8Array);
+    const pubKeyBuffer = Buffer.from(pubKey);
+    const pubKeyHash = proverWasm.poseidon.hashPubKey(pubKeyBuffer);
+    const index = pubKeyTree.indexOf(pubKeyHash);
+    const merkleProof = pubKeyTree.createProof(index);
+
+    // set up msgHashArray
+    const rndBuf = Buffer.from(sigmoji.PCD.proof.signedDigest, "hex");
+    const hash = sha256.create();
+    const msgHash = hash
+      .update(
+        Buffer.concat([
+          Buffer.from([0x19]),
+          Buffer.from("Attest counter pk2:\n", "utf8"),
+        ])
+      )
+      .update(rndBuf)
+      .hex();
+    const msgHashArray = hexToUint8Array(msgHash);
+
+    // extract v to be able to do efficient ECDSA
+    const v = findV(
+      r,
+      s,
+      Buffer.from(msgHashArray),
+      sigmoji.PCD.claim.pubkeyHex
+    );
+    const sig = `0x${r}${s}${v.toString(16)}`;
+
+    console.log("Proving...");
+    console.time("Full proving time");
+    const { proof, publicInput } = await proverWasm.prover.prove(
+      sig,
+      Buffer.from(msgHashArray),
+      merkleProof
+    );
+    console.timeEnd("Full proving time");
+
+    // update sigmoji with computed ZKP
+    sigmoji.ZKP = serializeSigmojiZKP(proof, publicInput);
+  }
+  return sigmoji;
+};
+
 export const makeProofs = async (proverWasm: ProverWasm) => {
   const treeDepth = 20;
   const pubKeyTree = new Tree(treeDepth, proverWasm.poseidon);
@@ -47,69 +112,14 @@ export const makeProofs = async (proverWasm: ProverWasm) => {
     pubKeyTree.insert(proverWasm.poseidon.hashPubKey(pubKeyBuffer));
   }
 
-  console.log(pubKeyTree);
-
-  // load in localStorage
+  // load in previous sigmojis
   const sigmojis = await loadSigmojis();
+  const sigmojisWithZKP = await Promise.all(
+    sigmojis.map((sigmoji) => addZKPToSigmoji(sigmoji, proverWasm, pubKeyTree))
+  );
 
-  // generate proofs for all the sigmojis
-  for (const sigmoji of sigmojis) {
-    if (sigmoji.ZKP === "") {
-      // set up signature
-      let { r, s } = parseDERSignature(sigmoji.PCD.proof.cleanedSignature);
-
-      // remove 00 at start of r, s if present
-      if (r.length == 66) {
-        r = r.slice(2);
-      }
-      if (s.length == 66) {
-        s = s.slice(2);
-      }
-
-      // setup pubkey + merkle proof
-      const secondaryPublicKeyRawUint8Array = hexToUint8Array(
-        sigmoji.PCD.claim.pubkeyHex
-      );
-      const pubKey = importPublic(secondaryPublicKeyRawUint8Array);
-      const pubKeyBuffer = Buffer.from(pubKey);
-      const pubKeyHash = proverWasm.poseidon.hashPubKey(pubKeyBuffer);
-      const index = pubKeyTree.indexOf(pubKeyHash);
-      const merkleProof = pubKeyTree.createProof(index);
-
-      // set up msgHashArray
-      const rndBuf = Buffer.from(sigmoji.PCD.proof.signedDigest, "hex");
-      const hash = sha256.create();
-      const msgHash = hash
-        .update(
-          Buffer.concat([
-            Buffer.from([0x19]),
-            Buffer.from("Attest counter pk2:\n", "utf8"),
-          ])
-        )
-        .update(rndBuf)
-        .hex();
-      const msgHashArray = hexToUint8Array(msgHash);
-
-      const v = findV(
-        r,
-        s,
-        Buffer.from(msgHashArray),
-        sigmoji.PCD.claim.pubkeyHex
-      );
-      const sig = `0x${r}${s}${v.toString(16)}`;
-
-      console.log("Proving...");
-      console.time("Full proving time");
-      const { proof, publicInput } = await proverWasm.prover.prove(
-        sig,
-        Buffer.from(msgHashArray),
-        merkleProof
-      );
-      console.timeEnd("Full proving time");
-
-      // save proof in localStorage
-      sigmoji.ZKP = serializeSigmojiZKP(proof, publicInput);
-      await updateSigmoji(sigmoji);
-    }
-  }
+  // update localStorage
+  window.localStorage["sigmojis"] = JSON.stringify(
+    await Promise.all(sigmojisWithZKP.map(serializeSigmoji))
+  );
 };
