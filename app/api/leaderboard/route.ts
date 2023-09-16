a30a55166ca51bbf477e2ba5915ba1b47d2e3652
AndrewCLu <andrewclu98@gmail.com>
2023-09-16 01:08:09 -0400

    send proofs to backend for verification

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -5,19 +5,74 @@ import {
 } from "@personaelabs/spartan-ecdsa";
 import prisma from "@/lib/prisma";
 
+export async function GET(request: Request) {
+  const leaderboard = await prisma.leaderboard.findMany({
+    orderBy: {
+      score: "desc",
+    },
+  });
+
+  return new NextResponse(JSON.stringify(leaderboard), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
 export async function POST(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const { pseudonym, score, serializedZKPArray } = Object.fromEntries(
-    searchParams.entries()
+  const bodyArrayBuffer = await request.arrayBuffer();
+  const bodyString = new TextDecoder().decode(bodyArrayBuffer);
+  const { pseudonym, score, serializedZKPArray } = JSON.parse(bodyString);
+
+  const zkpArray = (JSON.parse(serializedZKPArray) as string[]).map((zkp) =>
+    JSON.parse(zkp)
   );
 
+  const verified = await verifyProofs(zkpArray);
+  const scoreInt = parseInt(score);
+  const success = verified && scoreInt;
+
+  if (success) {
+    await addLeaderboardEntry(pseudonym, scoreInt);
+  }
+
+  return new NextResponse(JSON.stringify({ success }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+async function verifyProofs(
+  proofs: {
+    proof: Uint8Array;
+    publicInput: Uint8Array;
+  }[]
+): Promise<boolean> {
   const verifier = new MembershipVerifier({
     ...defaultPubkeyMembershipVConfig,
     enableProfiler: true,
   });
   await verifier.initWasm();
 
-  return new NextResponse(null, {
-    status: 200,
-  });
+  for (const proof of proofs) {
+    const verified = await verifier.verify(proof.proof, proof.publicInput);
+    if (!verified) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+async function addLeaderboardEntry(pseudonym: string, score: number) {
+  try {
+    const newEntry = await prisma.leaderboard.create({
+      data: {
+        pseudonym: pseudonym,
+        score: score,
+      },
+    });
+    console.log("New leaderboard entry:", newEntry);
+  } catch (error) {
+    console.error("Error adding leaderboard entry:", error);
+  }
 }
--- lib/zkProving.ts
+++ lib/zkProving.ts
@@ -23,7 +23,6 @@ export interface ProverWasm {
 export async function initWasm(): Promise<ProverWasm> {
   const poseidon = new Poseidon();
   await poseidon.initWasm();
-
   const prover = new MembershipProver({
     ...defaultPubkeyMembershipPConfig,
     enableProfiler: true,
@@ -40,62 +39,60 @@ export const addZKPToSigmoji = async (
   pubKeyTree: Tree
 ): Promise<Sigmoji> => {
   if (sigmoji.ZKP !== "") {
-    // set up signature
-    let { r, s } = parseDERSignature(sigmoji.PCD.proof.cleanedSignature);
-
-    // remove 00 at start of r, s if present
-    if (r.length == 66) {
-      r = r.slice(2);
-    }
-    if (s.length == 66) {
-      s = s.slice(2);
-    }
-
-    // setup pubkey + merkle proof
-    const secondaryPublicKeyRawUint8Array = hexToUint8Array(
-      sigmoji.PCD.claim.pubkeyHex
-    );
-    const pubKey = importPublic(secondaryPublicKeyRawUint8Array);
-    const pubKeyBuffer = Buffer.from(pubKey);
-    const pubKeyHash = proverWasm.poseidon.hashPubKey(pubKeyBuffer);
-    const index = pubKeyTree.indexOf(pubKeyHash);
-    const merkleProof = pubKeyTree.createProof(index);
-
-    // set up msgHashArray
-    const rndBuf = Buffer.from(sigmoji.PCD.proof.signedDigest, "hex");
-    const hash = sha256.create();
-    const msgHash = hash
-      .update(
-        Buffer.concat([
-          Buffer.from([0x19]),
-          Buffer.from("Attest counter pk2:\n", "utf8"),
-        ])
-      )
-      .update(rndBuf)
-      .hex();
-    const msgHashArray = hexToUint8Array(msgHash);
-
-    // extract v to be able to do efficient ECDSA
-    const v = findV(
-      r,
-      s,
-      Buffer.from(msgHashArray),
-      sigmoji.PCD.claim.pubkeyHex
-    );
-    const sig = `0x${r}${s}${v.toString(16)}`;
-
-    console.log("Proving...");
-    console.time("Full proving time");
-    const { proof, publicInput } = await proverWasm.prover.prove(
-      sig,
-      Buffer.from(msgHashArray),
-      merkleProof
-    );
-    console.timeEnd("Full proving time");
-
-    // update sigmoji with computed ZKP
-    sigmoji.ZKP = serializeSigmojiZKP(proof, publicInput);
+    return sigmoji;
   }
+
+  // set up signature
+  let { r, s } = parseDERSignature(sigmoji.PCD.proof.cleanedSignature);
+
+  // remove 00 at start of r, s if present
+  if (r.length == 66) {
+    r = r.slice(2);
+  }
+  if (s.length == 66) {
+    s = s.slice(2);
+  }
+
+  // setup pubkey + merkle proof
+  const secondaryPublicKeyRawUint8Array = hexToUint8Array(
+    sigmoji.PCD.claim.pubkeyHex
+  );
+  const pubKey = importPublic(secondaryPublicKeyRawUint8Array);
+  const pubKeyBuffer = Buffer.from(pubKey);
+  const pubKeyHash = proverWasm.poseidon.hashPubKey(pubKeyBuffer);
+  const index = pubKeyTree.indexOf(pubKeyHash);
+  const merkleProof = pubKeyTree.createProof(index);
+
+  // set up msgHashArray
+  const rndBuf = Buffer.from(sigmoji.PCD.proof.signedDigest, "hex");
+  const hash = sha256.create();
+  const msgHash = hash
+    .update(
+      Buffer.concat([
+        Buffer.from([0x19]),
+        Buffer.from("Attest counter pk2:\n", "utf8"),
+      ])
+    )
+    .update(rndBuf)
+    .hex();
+  const msgHashArray = hexToUint8Array(msgHash);
+
+  // extract v to be able to do efficient ECDSA
+  const v = findV(r, s, Buffer.from(msgHashArray), sigmoji.PCD.claim.pubkeyHex);
+  const sig = `0x${r}${s}${v.toString(16)}`;
+
+  console.log("Proving...");
+  console.time("Full proving time");
+  const { proof, publicInput } = await proverWasm.prover.prove(
+    sig,
+    Buffer.from(msgHashArray),
+    merkleProof
+  );
+  console.timeEnd("Full proving time");
+
+  // update sigmoji with computed ZKP
+  sigmoji.ZKP = serializeSigmojiZKP(proof, publicInput);
+
   return sigmoji;
 };
 
