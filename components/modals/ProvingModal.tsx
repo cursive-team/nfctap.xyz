9be96e1b60682d434c0090b1493468eccee0d6f0
AndrewCLu <andrewclu98@gmail.com>
2023-09-19 01:12:47 -0400

    time proofs generated by user

--- components/modals/ProvingModal.tsx
+++ components/modals/ProvingModal.tsx
@@ -23,17 +23,24 @@ import {
   serializeSigmojisInLocalStorage,
 } from "@/lib/localStorage";
 import { useRouter } from "next/navigation";
-import { serializeSigmoji } from "@/lib/types";
+
+enum ProvingState {
+  LOADING,
+  READY_TO_PROVE,
+  PROVING,
+  SUBMITTING,
+  SUCCESS,
+}
 
 export default function ProvingModal() {
   const router = useRouter();
-
+  const [provingState, setProvingState] = useState<ProvingState>(
+    ProvingState.LOADING
+  );
   const [wasm, setWasm] = useState<ProverWasm>();
-  const [pseudonym, setPseudonym] = useState<string>("");
-
-  const [proving, setProving] = useState<boolean>(false);
   const [score, setScore] = useState<number>(0);
   const [counter, setCounter] = useState(0);
+  const [pseudonym, setPseudonym] = useState<string>("");
 
   useEffect(() => {
     async function setup() {
@@ -48,52 +55,114 @@ export default function ProvingModal() {
     setup();
   });
 
-  const makeProofsWithCounter = async () => {
-    if (!wasm) return;
+  useEffect(() => {
+    if (wasm) {
+      setProvingState(ProvingState.READY_TO_PROVE);
+    }
+  }, [wasm]);
+
+  const makeProofsWithCounter = async (): Promise<{
+    serializedZKPArray: string[];
+    numUniqueProofs: Number;
+  }> => {
+    if (!wasm) throw new Error("WASM not initialized");
 
     setCounter(0);
-    setProving(true);
 
     const pubKeyTree = setupTree(wasm);
     const sigmojis = await loadSigmojis();
 
     // compute proofs in component to track progress for user
-    const wrappedSigmojis = sigmojis.map((sigmoji) =>
-      addZKPToSigmoji(sigmoji, wasm, pubKeyTree).then((result) => {
+    let numExistingProofs = 0;
+    const wrappedSigmojis = sigmojis.map(async (sigmoji) => {
+      if (sigmoji.ZKP) {
+        numExistingProofs++;
+      }
+      return addZKPToSigmoji(sigmoji, wasm, pubKeyTree).then((result) => {
         setCounter((prevCounter) => prevCounter + 1);
         return result;
-      })
-    );
+      });
+    });
     const sigmojisWithZKP = await Promise.all(wrappedSigmojis);
-    serializeSigmojisInLocalStorage(sigmojisWithZKP);
-    const serializedZKPArray = sigmojisWithZKP.map((s) => s.ZKP);
+    await serializeSigmojisInLocalStorage(sigmojisWithZKP);
+    return {
+      serializedZKPArray: sigmojisWithZKP.map((s) => s.ZKP),
+      numUniqueProofs: sigmojis.length - numExistingProofs,
+    };
+  };
 
-    fetch("/api/leaderboard", {
+  const proveAndSubmitScore = async () => {
+    setProvingState(ProvingState.PROVING);
+    const startTime = new Date().getTime();
+    const { serializedZKPArray, numUniqueProofs } =
+      await makeProofsWithCounter();
+    setProvingState(ProvingState.SUBMITTING);
+    const endTime = new Date().getTime();
+    const provingTime = endTime - startTime;
+
+    await fetch("/api/leaderboard", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        pseudonym: pseudonym,
-        score: score,
-        serializedZKPArray: serializedZKPArray,
+        pseudonym,
+        score,
+        serializedZKPArray,
+        provingTime,
       }),
     }).then(async (response) => {
-      setProving(false);
-
       if (response.status === 200) {
         const data = await response.json();
         if (data.success) {
+          setProvingState(ProvingState.SUCCESS);
           saveLeaderboardEntry({ pseudonym, score });
-          alert("Proof successfully verified!");
+          alert(
+            `Proof successfully verified! Generated ${numUniqueProofs} new proofs in: ${provingTimeString(
+              provingTime
+            )}.`
+          );
           router.push("/home");
         } else {
-          alert("Proof failed to verify.");
+          setProvingState(ProvingState.READY_TO_PROVE);
+          alert("The proof you submitted was invalid.");
         }
       } else {
+        setProvingState(ProvingState.READY_TO_PROVE);
         alert("Error submitting proof to leaderboard.");
       }
     });
+  };
+
+  const provingTimeString = (duration: number): string => {
+    const seconds = Math.floor((duration / 1000) % 60);
+    const minutes = Math.floor((duration / (1000 * 60)) % 60);
+    const hours = Math.floor(duration / (1000 * 60 * 60));
+
+    let timeString = `${seconds}s`;
+    if (minutes > 0 || hours > 0) {
+      timeString = `${minutes}m ${timeString}`;
+    }
+    if (hours > 0) {
+      timeString = `${hours}h ${timeString}`;
+    }
+
+    return timeString;
+  };
+
+  const buttonText = () => {
+    switch (provingState) {
+      case ProvingState.LOADING:
+        return "LOADING...";
+      case ProvingState.READY_TO_PROVE:
+        return "PROVE IT!";
+      case ProvingState.PROVING:
+        return `PROVING ${((counter / score) * 100.0).toFixed(1)}%`;
+      case ProvingState.SUBMITTING:
+        return "SUBMITTING PROOF...";
+      case ProvingState.SUCCESS:
+        return "SUCCESS!";
+    }
   };
 
   return (
@@ -116,16 +185,8 @@ export default function ProvingModal() {
 
           <Input header="Pseudonym" value={pseudonym} setValue={setPseudonym} />
 
-          <PrimaryLargeButton
-            onClick={() => (wasm ? makeProofsWithCounter() : {})}
-          >
-            <PrimaryFontBase1>
-              {!wasm
-                ? "LOADING..."
-                : !proving
-                ? "PROVE IT!"
-                : `PROVING ${((counter / score) * 100.0).toFixed(1)}%`}
-            </PrimaryFontBase1>
+          <PrimaryLargeButton onClick={wasm ? proveAndSubmitScore : () => {}}>
+            <PrimaryFontBase1>{buttonText()}</PrimaryFontBase1>
           </PrimaryLargeButton>
         </InnerContainer>
       </OuterContainer>
