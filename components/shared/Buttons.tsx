c2b9db3c3fe0a4ca6c4177cdcca82e96b457ccc6
AndrewCLu <andrewclu98@gmail.com>
2023-09-19 00:07:51 -0400

    highlight user leaderboard submissions, cumulative ranks, button animation

--- components/shared/Buttons.tsx
+++ components/shared/Buttons.tsx
@@ -12,7 +12,7 @@ export const CollectButton = styled.div`
   background: var(--snow-flurry-200);
 `;
 
-export const PrimaryLargeButton = styled.div`
+export const PrimaryLargeButton = styled.button`
   display: flex;
   width: 264px;
   height: 42px;
@@ -23,6 +23,11 @@ export const PrimaryLargeButton = styled.div`
   flex-shrink: 0;
   border-radius: 6px;
   background: var(--snow-flurry-200);
+  transition: transform 0.2s ease; // smooth transform transition
+
+  &:active {
+    transform: scale(0.95); // scale down a bit when clicked
+  }
 `;
 
 export const SecondaryLargeButton = styled.div`
