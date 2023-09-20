a599aca4542b738ecbef8d1096a6adcb856a385f
AndrewCLu <andrewclu98@gmail.com>
2023-09-20 13:33:24 -0400

    refactor recovery screen, display recovery alerts

--- app/recover/page.tsx
+++ app/recover/page.tsx
@@ -1,39 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter, useParams } from "next/navigation";
-import { Sigmoji, deserializeSigmoji } from "@/lib/types";
-import { updateSigmojiList } from "@/lib/localStorage";
+import RecoverScreen from "@/components/screens/RecoverScreen";
 
 export default function RecoverPage() {
-  const router = useRouter();
-  const params = useParams();
-
-  useEffect(() => {
-    const recoverBackup = async () => {
-      if (typeof window !== "undefined") {
-        const urlParams = new URLSearchParams(location.search);
-        const sigmojis = await getCollectionFromParams(urlParams);
-        await updateSigmojiList(sigmojis);
-        router.push("/home");
-      }
-    };
-    recoverBackup();
-  }, [params, router]);
-
-  return <></>;
-}
-
-function getCollectionFromParams(params: URLSearchParams): Promise<Sigmoji[]> {
-  const collection = params.get("collection");
-  if (!collection) {
-    return Promise.resolve([]);
-  }
-
-  const serializedSigmojis = JSON.parse(collection);
-  if (!Array.isArray(serializedSigmojis)) {
-    return Promise.resolve([]);
-  }
-
-  return Promise.all(serializedSigmojis.map(deserializeSigmoji));
+  return <RecoverScreen />;
 }
